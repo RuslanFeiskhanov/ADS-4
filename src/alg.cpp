9dd0dc8d235a7c6f2889d9d69138e36788a9dd64
RuslanFeiskhanov <99216168+RuslanFeiskhanov@users.noreply.github.com>
2022-03-11 22:47:53 +0300

    www

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,37 +13,29 @@ int countPairs2(int *arr, int len, int value) {
     for (int i = 0; i < len; i++)
         for (int j = len - 1; j > i; j--)
             if (arr[i] + arr[j] == value)
-                count++;        
+                count++;
     return count;
 }
-
-int binar (int *arr, int l, int r, int x) {
-    int mid = (l + r) / 2;
-    int c = 0;
-    int lt = l, rt = r;
-    while (lt < rt) {
-        if (x > arr[mid]) {
-            lt = mid+1;
-            mid = (lt + rt) / 2;
-        }
-        else
-        if (x < arr[mid]) {
-            rt = mid-1;
-            mid = (lt + rt) / 2;
-        }
-        else
+int binar (int *arr, int l, int r, int x) {   
+    int lt = l, rt = r, mid = 0;;
+    while (lt <= rt) {
+        mid = (lt + rt) / 2;
         if (arr[mid] == x) {
             return 1;
             break;
         }
+        if (x < arr[mid])
+            rt = mid-1;
+        else
+        if (x > arr[mid])
+            lt = mid+1;
     }
-    return c;
+    return 0;
 }
-
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len-1; i++) {
-       count += binar(arr, 0, len, value-arr[i]);
+    for (int i = 0; i < len; i++) {
+       count += binar(arr, i, len -1, value - arr[i]);
     }
     return count;
 }
